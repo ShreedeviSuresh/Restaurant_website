fa8c616413a69df5f20b4e42aea07e56cf7a39e0
ShreedeviSuresh <145690852+ShreedeviSuresh@users.noreply.github.com>
2023-09-21 20:42:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Restaurant_website
-A restaurant website which helps to place order in an efficient and convienient way.
+# restuarant_website
+
+
+This website is designed for the 'Let's Ketchup' restaurant. the website is developed using HTML,CSS and JavaScript. The customer can view the webite and can navigate to different sections of the website with the naviagtion bar. The wesite also contains a map for the directions to reach. It has an enticing menu presentation with high quality food images and a gallery showcasing the restaurant's ambience. It also has an option for the customers to make reservation through reservation form.
---
+++ script.js
@@ -0,0 +1,122 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.footer-form');
+  const success = document.getElementById('success');
+  const danger = document.getElementById('danger');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const msg = document.getElementById('msg').value;
+
+    if (name === '' || email === '' || msg === '') {
+      danger.style.display = 'block';
+      success.style.display = 'none';
+    } else {
+      success.style.display = 'block';
+      danger.style.display = 'none';
+
+      setTimeout(() => {
+        name.value = '';
+        email.value = '';
+        msg.value = '';
+        success.style.display = 'none';
+      }, 2000);
+    }
+  });
+});
+
+/**
+ * header sticky & back to top
+ */
+
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+
+/**
+ * search box toggle
+ */
+
+const searchBtn = document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-container]");
+const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+
+const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
+
+for (let i = 0; i < searchBoxElems.length; i++) {
+  searchBoxElems[i].addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * move cycle on scroll
+ */
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
+
